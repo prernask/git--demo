07b3dda21bb67cd9c0a83aea947e710f9af22095
Prerana Karande <91388114+prernask@users.noreply.github.com>
2025-02-12 14:05:37 +0530

    Add files via upload

---
+++ new.py
@@ -0,0 +1,27 @@
+#git revert 
+
+#git status
+#git branch create a new branch
+#git checkout -b dev cmd
+#commit : to add file into git
+
+#git initialize
+#initialize an empty git repository
+
+#git status 
+#gives the infor of the current state of the git res
+
+#git checkout -b <branch name>
+#switching to new branch , if you want to switch to an exiting branch then use git checkout branch with -b
+
+#git add <filename>
+# used to stage the untracked file
+
+#git branch
+#-gives the info / list of all the branches in git repo
+
+#git commit -m "<your message>"
+# used to track the staged files after being added by git add
+
+#git restore --staged <filename>
+# used to untracked/unstaged the staged file
